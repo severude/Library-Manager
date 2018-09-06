13d2cd219cec0346f6fba3dd52293c35c9be7ab9
Dale Severude <dseverude@msn.com>
2018-09-05 17:44:13 -0700

    Added book error validation

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
-    res.render('error');
+    res.render('not_found');
 });
   
 module.exports = app;
--- models/book.js
+++ models/book.js
@@ -1,10 +1,38 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Book = sequelize.define('Book', {
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    genre: DataTypes.STRING,
-    first_published: DataTypes.INTEGER
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Title is required"
+        }
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Author is required"
+        }
+      }
+    },
+    genre: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Genre is required"
+        }
+      }
+    },
+    first_published: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {
+          msg: "First Published should be a year"
+        }
+      }
+    }
   }, { 
     timestamps: false
   });
